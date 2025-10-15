aaa7323e33a795f51184361dbbbbb7198c52072d
Obrempong-1 <obrempong.kow@gmail.com>
2025-10-15 21:21:27 +0100

    Update

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -11,13 +11,13 @@ import { getStorage } from "firebase/storage"; // Import getStorage
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-};
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -29,4 +29,3 @@ const functions = getFunctions(app);
 const storage = getStorage(app); // Initialize Storage
 
 export { db, auth, functions, storage }; // Export storage
-export default firebaseConfig;
